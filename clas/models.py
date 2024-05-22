4659ebf15c3f9055d1d5ba38463bf0475adc8c0c
wabba <maiorlegal@live.com>
2024-05-22 00:38:15 -0300

    removido as tabelas de livros e edicao do app clas

--- clas/models.py
+++ clas/models.py
@@ -2,46 +2,6 @@ from django.utils.translation import gettext_lazy as _
 from django.urls import reverse
 from django.db import models
 from django.utils import timezone
-from .models import Edicao
-
-
-
-# Cria a tabela de Edição
-class Edicao(models.Model):
-    nome = models.CharField(_("Nome"), max_length=100)
-    criado_ts = models.DateTimeField(null=False, blank=False, default=timezone.now)
-    atualizado_ts = models.DateTimeField(default=timezone.now)
-
-    class Meta:
-        verbose_name = _("edição")
-        verbose_name_plural = _("edições")
-
-    def __str__(self):
-        return self.nome
-
-    def get_absolute_url(self):
-        return reverse("Edicao_detail", kwargs={"pk": self.pk})
-
-
-# Cria a tabela de Livro
-class Livro(models.Model):
-    nome = models.CharField(_("Nome"), max_length=100, blank=False, null=False)
-    edicao = models.ForeignKey(Edicao, verbose_name=_("Edição"), on_delete=models.CASCADE)
-    editora = models.CharField(_("Editora"), max_length=100)
-    revisao = models.CharField(_("Revisão"), max_length=50)
-    isbn = models.CharField(_("ISBN"), max_length=13)  # Adicionado max_length
-    criado_ts = models.DateTimeField(null=False, blank=False, default=timezone.now)
-    atualizado_ts = models.DateTimeField(default=timezone.now)
-
-    class Meta:
-        verbose_name = _("livro")
-        verbose_name_plural = _("livros")
-
-    def __str__(self):
-        return f"{self.nome} - {self.edicao.nome}"
-
-    def get_absolute_url(self):
-        return reverse("Livro_detail", kwargs={"pk": self.pk})
 
 
 # Cria a tabela de Disciplinas
