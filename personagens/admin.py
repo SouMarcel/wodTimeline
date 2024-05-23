795275dbe821abf46367fe461800bed52e1fc6b1
wabba <maiorlegal@live.com>
2024-05-22 21:23:20 -0300

    criado as configurações para que as models possam ser administradas no acesso admin do django

--- personagens/admin.py
+++ personagens/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Livro, Personagem
 
-# Register your models here.
+@admin.register(Livro)
+class LivroAdmin(admin.ModelAdmin):
+    # Lista de campos a serem exibidos na interface de administração
+    list_display = ('nome', 'edicao', 'editora', 'revisao', 'isbn', 'criado_em', 'atualizado_em')
+    # Campos de pesquisa para facilitar a localização de registros na interface de administração
+    search_fields = ('nome', 'editora', 'isbn')
+
+@admin.register(Personagem)
+class PersonagemAdmin(admin.ModelAdmin):
+    # Lista de campos a serem exibidos na interface de administração
+    list_display = ('nome', 'alcunha', 'nascimento', 'cla', 'criado_em', 'atualizado_em')
+    # Campos de pesquisa para facilitar a localização de registros na interface de administração
+    search_fields = ('nome', 'alcunha')
+    # Filtros disponíveis na interface de administração para filtrar os registros por clã
+    list_filter = ('cla',)
