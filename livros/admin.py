3bca1562326ecd18c464a54c33bd919b807ba8c1
wabba <maiorlegal@live.com>
2024-05-27 16:22:58 -0300

    adicionado os tabelas adicionadas ao modeulo admin

--- livros/admin.py
+++ livros/admin.py
@@ -1,3 +1,17 @@
-from django.contrib import admin
+# admin.py
 
-# Register your models here.
+from django.contrib import admin
+from .models import Edicao, Livro
+
+class EdicaoAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'criado_em', 'atualizado_em')
+    search_fields = ('nome',)
+
+class LivroAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'edicao', 'editora', 'revisao', 'isbn', 'criado_em', 'atualizado_em')
+    list_filter = ('edicao', 'editora')
+    search_fields = ('nome', 'editora', 'isbn')
+
+# Registro dos modelos no admin com personalização
+admin.site.register(Edicao, EdicaoAdmin)
+admin.site.register(Livro, LivroAdmin)
