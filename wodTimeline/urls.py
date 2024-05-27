024b0ff25923de2302b8d5d682991cc0669d5840
wabba <maiorlegal@live.com>
2024-05-27 18:42:57 -0300

    criado a configuração base do app diciplinas

--- wodTimeline/urls.py
+++ wodTimeline/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('livros/', include('livros.urls')),
+    path('disciplinas/', include('disciplinas.urls')),
 ]
