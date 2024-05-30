ed02a9e0b14fefe3fadadbd7e4cc912082da771c
wabba <maiorlegal@live.com>
2024-05-30 03:50:35 -0300

    configurado personagens_detail.html, e a urls

--- personagens/urls.py
+++ personagens/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path("", views.home, name="home"),
-    path('lista/', views.personagem_list, name='personagens_list'),
+    path('lista/', views.personagem_list, name='personagem_list'),
     path('detail/<int:pk>/', views.personagem_detail, name='personagens_detail'),
     path('novo/', views.personagem_novo, name='personagem_novo')
     
