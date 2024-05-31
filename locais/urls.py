07d01a7a60bc4e9c6958ad067378b1c0545480b9
wabba <maiorlegal@live.com>
2024-05-31 04:52:17 -0300

    criado e configurado o modulo locais, paginas lista, novo e detalhes criadas e configuradas

--- locais/urls.py
+++ locais/urls.py
@@ -4,10 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('lista/', views.local_list, name='local_list'),
-#    path("novo/", views.local_novo, name="local_novo"),
+    path("novo/", views.local_novo, name="local_novo"),
+    path('detalhes/<int:pk>', views.local_detail, name='local_detail')
 ]
-
-
-
-"""     path('lista/', views.local_list, name='local_list'),
-    path('novo/', views.local_novo, name='local_novo'), """
--- locais/views.py
+++ locais/views.py
@@ -1,22 +1,43 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Local
 from .forms import LocalForm
 
-# Create your views here.
-def home(request):
-    return render(request, 'locais/home.html')
+# Cria suas views aqui.
+def home(request):  # Define a view chamada "home"
+    return render(request, 'locais/home.html')  # Renderiza o template 'home.html' na pasta 'locais'
 
-# view para listagem de local
-def local_list(request):
-    local = Local.objects.all() # variavel local = a todos os itens na tabela Local
+# View para listagem de locais
+def local_list(request):  # Define a view chamada "local_list" para listar os locais
+    local = Local.objects.all()  # Atribui à variável 'local' todos os itens da tabela 'Local'
     
-    context = { # cria a variavel para passar 2 outras variaveis
-        'local': local, # variavel que sera passada ao html referente a tabela Local
-        'title': 'Lista de locais'  # variavel com um nome para a pagina
+    context = {  # Cria um dicionário de contexto para passar ao template
+        'local': local,  # Passa a variável 'local' (todos os itens da tabela 'Local') para o template
+        'title': 'Lista de locais'  # Passa uma string 'Lista de locais' como título da página
     }
-    return render(request, 'locais/locais_list.html', context) # retorna a pagina de listagem e as variaveis no contexto
+    return render(request, 'locais/locais_list.html', context)  # Renderiza o template 'locais_list.html' na pasta 'locais' com as variáveis do contexto
+
+def local_novo(request):  # Define a view para adicionar um novo local
+    if request.method == 'POST':  # Verifica se o método da requisição é POST
+        form = LocalForm(request.POST)  # Instancia o formulário com os dados enviados
+        if form.is_valid():  # Verifica se o formulário é válido
+            novo_local = form.save()  # Salva os dados do formulário no banco de dados
+            if request.POST.get('action') == 'save_and_add_new':
+                return redirect('local_novo')
+            return redirect('local_detail', pk=novo_local.pk)  # Redireciona para a detalhes da nova entrada
+    else:  # Se o método da requisição não for POST
+        form = LocalForm()  # Instancia um formulário vazio
+    
+    context = {  # Cria o dicionário de contexto para passar ao template
+        'form': form,  # Passa o formulário para o template
+        'title': 'Adicionar novo Local'  # Passa o título da página para o template
+    }
+    return render(request, 'locais/locais_form.html', context)  # Renderiza o template com o contexto
 
 
-def local_novo(request):
-    if request.method == 'POST':
-        form = LocalForm(request.POST) 
+def local_detail(request, pk):
+    local = get_object_or_404(Local,pk=pk)
+    context = {
+        'local': local,
+        'title': f'Detalhes do Local: {local.nome} - {local.livro}'
+    }
+    return render(request, 'locais/locais_detail.html', context)
